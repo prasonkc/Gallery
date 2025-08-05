0153d54afd2f3baf6d79ae6e6f1d2ab73177a6f7
Prason <prasonkc07@gmail.com>
2025-08-04 23:51:43 -0400

    Js edit

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
+// const dropZone = document.querySelector('.drop_zone');
 
-const gallery = document.querySelector('.gallery');
-document.createElement('div').className = 'image_container';
-document.createElement('img').src = `${imageSrc}`;
+// dropZone.addEventListener('dragover', (e) => {
+//     e.preventDefault();
+//     dropZone.classList.add('dragover');
+// });
+
+// dropZone.addEventListener('dragleave', () => {
+//     e.preventDefault(); 
+//     dropZone.classList.remove('dragover'); 
+// });
+
+// dropZone.addEventListener('drop', (e) =>{
+//     e.preventDefault
+//     dropZone.classList.remove('dragover');
+
+//     const file = e.dataTransfer.files[0];
+//     if (file && file.type.startsWith('image/')) {
+//         const reader = new FileReader();
+//         reader.onload = (e) => {
+//             const container = document.createElement('div');
+//             container.className = 'image_container';
+
+//             const img = document.createElement('img');
+//             img.src = e.target.result;
+
+//              container.appendChild(img);
+//              gallery.insertBefore(container, dropZone);
+//         }
+//     }
+//     reader.readAsDataURL(file);
+// })
+
+
