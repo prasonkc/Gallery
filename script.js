d8edb05b4d0e89a54cecfb5bd1f4bd27ef99cc36
Prason <prasonkc07@gmail.com>
2025-08-05 00:06:13 -0400

    fixed js bugs

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
-// const dropZone = document.querySelector('.drop_zone');
+const dropZone = document.querySelector('.drop_zone');
+const gallery = document.querySelector('.gallery');
 
-// dropZone.addEventListener('dragover', (e) => {
-//     e.preventDefault();
-//     dropZone.classList.add('dragover');
-// });
+dropZone.addEventListener('dragover', (e) => {
+    e.preventDefault();
+    dropZone.classList.add('dragover');
+});
 
-// dropZone.addEventListener('dragleave', () => {
-//     e.preventDefault(); 
-//     dropZone.classList.remove('dragover'); 
-// });
+dropZone.addEventListener('dragleave', (e) => {
+    e.preventDefault(); 
+    dropZone.classList.remove('dragover'); 
+});
 
-// dropZone.addEventListener('drop', (e) =>{
-//     e.preventDefault
-//     dropZone.classList.remove('dragover');
+  dropZone.addEventListener('drop', (e) => {
+      e.preventDefault();
+      dropZone.classList.remove('dragover');
 
-//     const file = e.dataTransfer.files[0];
-//     if (file && file.type.startsWith('image/')) {
-//         const reader = new FileReader();
-//         reader.onload = (e) => {
-//             const container = document.createElement('div');
-//             container.className = 'image_container';
+      const file = e.dataTransfer.files[0];
+      if (file && file.type.startsWith('image/')) {
+          const reader = new FileReader();
+          reader.onload = (e) => {
+              const container = document.createElement('div');
+              container.className = 'image_container';
 
-//             const img = document.createElement('img');
-//             img.src = e.target.result;
+              const img = document.createElement('img');
+              img.src = e.target.result;
 
-//              container.appendChild(img);
-//              gallery.insertBefore(container, dropZone);
-//         }
-//     }
-//     reader.readAsDataURL(file);
-// })
+              container.appendChild(img);
+              gallery.insertBefore(container, dropZone);
+          };
+          reader.readAsDataURL(file);
+      }
+  });
 
 
